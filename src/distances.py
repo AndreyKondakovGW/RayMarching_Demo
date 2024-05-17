dcc94650923b68ff70825588c76340d6842cff11
Joao Anastacio <almeida.jar97@gmail.com>
2024-05-17 13:58:32 -0300

    new shapes

--- src/distances.py
+++ src/distances.py
@@ -1,4 +1,45 @@
+import numpy as np
+import numpy.typing as npt
 
+def distance_from_sphere(point:npt.ArrayLike,radius:float) -> float:
+    return np.linalg.norm(point)-radius
 
-def distance_from_sphere():
+def distance_from_box(point:npt.ArrayLike,half_sides:npt.ArrayLike) -> float:
+    q= np.abs(point)-half_sides
+    return np.linalg.norm(np.maximum(q,0)) + np.min((np.max(q),0))
+
+def distance_from_frame_box(point:npt.ArrayLike,half_sides:npt.ArrayLike, thickness: float) -> float:
+    point=np.abs(point)-half_sides
+    q= np.abs(np.add(point,thickness))-thickness
+    return np.min((np.linalg.norm(np.maximum([point[0],q[1],q[2]],0.0))+min(max(point[0],q[1],q[2]),0.0),np.linalg.norm(np.maximum([q[0],point[1],q[2]],0.0))+min(max(q[0],point[1],q[2]),0.0),np.linalg.norm(np.maximum([q[0],q[1],point[2]],0.0))+min(max(q[0],q[1],point[2]),0.0)))
+
+def distance_from_round_box(point:npt.ArrayLike,half_sides:npt.ArrayLike, rounding: float) -> float:
+    q= np.abs(point)-half_sides +rounding
+    return np.linalg.norm(np.maximum(q,0)) + np.min((np.max(q),0))-rounding
+
+def distance_from_torus(point:npt.ArrayLike,radi:npt.ArrayLike) -> float:
+    q=np.array([np.linalg.norm([point[0],point[2]])-radi[0],point[1]])
+    return np.linalg.norm(q)-radi[1]
+
+def distance_from_cylinder(point:npt.ArrayLike,radius:float, height: float):
+    d=np.abs(np.array([np.linalg.norm([point[0],point[2]]),point[1]]))-np.array([radius,height])
+    return min(max(d),0)+np.linalg.norm(np.maximum(d,0))
+
+def distance_from_cone(point:npt.ArrayLike,c:npt.ArrayLike, height: float):
+    q=height*np.array([c[0]/c[1],-1])
+    w=np.array([np.linalg.norm([point[0],point[2]]),point[1]])
+    a= w-q*np.clip(np.dot(w,q)/np.dot(q,q),0,1)#is a a vec2?
+    b=w-q*np.array([np.clip(w[0]/q[0],0,1),1])
+    k=np.sign(q[1])
+    d=min(np.dot(a,a),np.dot(b,b))
+    s=np.max((k*(w[0]*q[1]-w[1]*q[0]),k*(w[1]-q[1])))
+    return np.sqrt(d)*np.sign(s)
+
+def distance_from_plane():
+    pass
+
+def distance_from_prism():
+    pass
+
+def distance_from_capsule():
     pass
--- src/ray_marchobject.py
+++ src/ray_marchobject.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import numpy as np
+import src.distances as distances
 
 class RayMarchObject(ABC):
     @abstractmethod
@@ -17,11 +18,11 @@ class Sphere(RayMarchObject):
         self.color = color
 
     def distance2point(self, point):
-        return np.linalg.norm(self.center - point) - self.radius
-    
+        return distances.distance_from_sphere(self.center-point,self.radius)
+
     def get_color(self):
         return self.color
-    
+
 
 class FuzzySphere(RayMarchObject):
     def __init__(self, center, radius, color = np.array([1,0,0])):
@@ -30,8 +31,89 @@ class FuzzySphere(RayMarchObject):
         self.color = color
 
     def distance2point(self, point):
-        scaler = 5.0 
+        scaler = 5.0
         displacement = np.sin(scaler * point[0]) * np.sin(scaler* point[1]) * np.sin(scaler* point[2]) * 0.25 * self.radius
         return np.linalg.norm(self.center - point) - self.radius + displacement
     def get_color(self):
         return self.color
+
+
+class Box(RayMarchObject):
+    def __init__(self, center, half_sides, color = np.array([1,0,0])):
+        self.center = center
+        self.half_sides = half_sides
+        self.color = color
+
+    def distance2point(self, point):
+        d=distances.distance_from_box(self.center-point,self.half_sides)
+        #print(d)
+        return d
+
+    def get_color(self):
+        return self.color
+
+
+class RoundBox(RayMarchObject):
+    def __init__(self, center, half_sides, rounding, color = np.array([1,0,0])):
+        self.center = center
+        self.half_sides = half_sides
+        self.roundig=rounding
+        self.color = color
+
+    def distance2point(self, point):
+        return distances.distance_from_round_box(self.center-point,self.half_sides,self.roundig)
+
+    def get_color(self):
+        return self.color
+
+class FrameBox(RayMarchObject):
+    def __init__(self, center, half_sides, thickness, color = np.array([1,0,0])):
+        self.center = center
+        self.half_sides = half_sides
+        self.thickness=thickness
+        self.color = color
+
+    def distance2point(self, point):
+        return distances.distance_from_frame_box(self.center-point,self.half_sides,self.thickness)
+
+    def get_color(self):
+        return self.color
+
+class Torus(RayMarchObject):
+    def __init__(self, center, radi, color = np.array([1,0,0])):
+        self.center = center
+        self.radi = radi
+        self.color = color
+
+    def distance2point(self, point):
+        return distances.distance_from_torus(self.center-point,self.radi)
+
+    def get_color(self):
+        return self.color
+
+class Cylinder(RayMarchObject):
+    def __init__(self, center, radius,height ,color = np.array([1,0,0])):
+        self.center = center
+        self.radius = radius
+        self.height = height
+        self.color = color
+
+    def distance2point(self, point):
+        return distances.distance_from_cylinder(self.center-point,self.radius,self.height)
+
+    def get_color(self):
+        return self.color
+
+class Cone(RayMarchObject):
+    def __init__(self, center, c, height, color = np.array([1,0,0])):
+        self.center = center
+        self.c = c
+        self.height = height
+        self.color = color
+
+    def distance2point(self, point):
+        return distances.distance_from_cone(self.center-point,self.c,self.height)
+
+    def get_color(self):
+        return self.color
+
